5012aab9bb1e15531965b71803b563a7f0a87b56
Roger Ye <shugaoye@yahoo.com>
2022-04-15 23:17:35 +0800

    preview14

---
+++ MauiBlazorApp1/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using MauiBlazorApp1.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace MauiBlazorApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .RegisterBlazorMauiWebView()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddBlazorWebView();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
